717db8ae87f1775dbdae25675781518c296693ae
nam_45 <nam_1999_x@yahoo.com.vn>
2019-06-28 19:33:39 +0700

    updated

--- server/routes.js
+++
@@ -1,9 +0,0 @@
-import examplesRouter from './api/controllers/examples/router';
-import userRouter from './api/controllers/users/router';
-import productRouter from './api/controllers/products/router';
-
-export default function routes(app) {
-  app.use('/api/v1/examples', examplesRouter);
-  app.use('/api/v1/users', userRouter);
-  app.use('/api/v1/products', productRouter);
-}
