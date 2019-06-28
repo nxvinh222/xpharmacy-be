b8255c4677118070eafaa167eb455a314995e9f5
nam_45 <nam_1999_x@yahoo.com.vn>
2019-06-28 19:42:28 +0700

    product updated

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
 import examplesRouter from './api/controllers/examples/router';
 import userRouter from './api/controllers/users/router';
 import productRouter from './api/controllers/products/router';
-
+import authRouter from './api/controllers/auth/router';
 export default function routes(app) {
   app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/users', userRouter);
+  app.use('/api/v1/login',authRouter);
   app.use('/api/v1/products', productRouter);
 }
