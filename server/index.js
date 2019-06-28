01be718e3ede7ac68c28c7b6548e7c8fcdd23b97
unknown <nxvinh222@gmail.com>
2019-06-28 20:52:48 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import './common/env';
 import Server from './common/server';
 import routes from './routes';
-import mongoose from 'mongoose';
 
 export default new Server().router(routes).listen(process.env.PORT);
