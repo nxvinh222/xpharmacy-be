dc7f23491efff34b308ab72c2a840bd6fefbb790
unknown <nxvinh222@gmail.com>
2019-07-03 23:25:57 +0700

    .

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,15 @@ import examplesRouter from './api/controllers/examples/router';
 import userRouter from './api/controllers/users/router';
 import productRouter from './api/controllers/products/router';
 import authRouter from './api/controllers/auth/router';
+import cors from 'cors';
+
 export default function routes(app) {
+  app.use(
+    cors(function(req, callback) {
+      const corsOptions = { origin: true, credentials: true, exposedHeaders: ["X-Total-Count"] };
+      callback(null, corsOptions);
+    }),
+  );
   app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/users', userRouter);
   app.use('/api/v1/login',authRouter);
